85d939121472b8113199be8d585bd7d255e0f02e
Sbeesl <skylarbeesley@yahoo.com>
2026-02-27 18:25:52 -0600

    Initial Commit

---
+++ MemoryMatchGame/MemoryMatchGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryMatchGameApp.swift
+//  MemoryMatchGame
+//
+//  Created by Skylar Beesley on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct MemoryMatchGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
